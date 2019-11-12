78c14f1afef06ceccd9f6247405164df3c93a7a3
nikmedoed <muromcevn@gmail.com>
2019-11-12 18:44:00 +0300

    names base checker OK

--- checkBase.py
+++ checkBase.py
@@ -1,41 +1,28 @@
 from selenium import webdriver
 from time import sleep
-import random
-from datetime import datetime
 
-def getLinks(name, link):
+def checkBase(base):
     browser = webdriver.Chrome()
-    browser.get(link)
-    with open("base.txt","r"):
-
-    sleep(random.randint(5, 20))
-    links = []
-    print("start")
-    while 1:
-        lis = browser.find_elements_by_xpath('//div[4]/div[1]/div[1]/a')
-        links.extend(list(map(lambda x: x.get_attribute('href'), lis)))
-
-        # https://riptutorial.com/selenium-webdriver/example/28140/scrolling-using-python
-        # if lis: lis[random.randint(len(lis))].
-
-
-        next = browser.find_element_by_class_name('n-pager__button-next').get_attribute('href')
-        while next:
-            try:
-                sleep(random.randint(1, 250))
-                browser.get(next)
-                print(datetime.now(), "- next")
-            except:
-                print('хьюстон, нас банят эгэйн')
-            else:
-                break
-    links = list(map(lambda x: x.split('?')[0] + "/spec?track=tabs", links))
-    with open('links\\' + name + '.txt', 'w') as s:
-        s.write("\n".join(links))
+    browser.get('https://web.whatsapp.com/')
+    input("enter как авторизируешься")
+    inp = browser.find_element_by_xpath('//div[1]/div/label/input')
+    res = []
+    inp.click()
+    for i in base:
+        inp.send_keys(i)
+        sleep(2)
+        pers = browser.find_elements_by_xpath('//div[2]/div[1]/div/span/span/span')
+        te = '%s\t%s' % (i.split(' - ')[1], len(pers))
+        print(te)
+        res.append(te)
+        inp.clear()
     browser.close()
-    return links
+    print(res)
+    with open('verificated.txt', 'w', encoding="utf-8") as f:
+        f.write('\n'.join(res))
+    return res
 
 if __name__ == "__main__":
-    print(getLinks("Стиралки",
-                   "https://market.yandex.ru/catalog--stiralnye-mashiny/54913/list?onstock=1&local-offers-first=0"))
-
+    with open("../base.txt", "r", encoding="utf8") as b:
+        base = b.read().split("\n")
+    checkBase(base)
